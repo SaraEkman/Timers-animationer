543408d0a48aab60fc558fe550673debda00da52
tovehydmark <tovehydmark@gmail.com>
2021-12-07 12:26:57 +0100

    hus flyttar på sig, hero kan hoppa

--- main.js
+++ main.js
@@ -1,1 +1,67 @@
-console.log("hej");
+let hero = document.getElementById("hero");
+let bottom = 200;
+let left = 20;
+let counter = 0;
+
+function jump() {
+    let timer = setInterval(function() {
+        counter++
+        bottom += 10;
+
+        if (counter == 10) {
+            clearInterval(timer)
+
+            let timerdown = setInterval(function() {
+                if (bottom == 200) { clearInterval(timerdown) }
+                bottom -= 10;
+                hero.style.bottom = bottom + "px";
+            }, 30);
+        };
+
+        hero.style.bottom = bottom + "px";
+
+    }, 30);
+    counter = 0;
+    console.log(counter);
+}
+
+
+document.addEventListener("keyup", function(evt) {
+    switch (evt.key) {
+        // case "ArrowRight":
+        //     left += 10;
+        //     hero.style.left = left + "px";
+        //     break;
+        // case "ArrowLeft":
+        //     left -= 10;
+        //     hero.style.left = left + "px";
+        //     break;
+        case "ArrowUp":
+            jump();
+            break;
+
+        default:
+            break;
+    }
+})
+
+var id = null;
+
+function myMove() {
+    var elem = document.getElementById("hole");
+    var pos = 1100;
+    clearInterval(id);
+    id = setInterval(frame, 15);
+
+    function frame() {
+        if (pos == -50) {
+            clearInterval(id);
+            myMove();
+        } else {
+            pos--;
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+myMove();
